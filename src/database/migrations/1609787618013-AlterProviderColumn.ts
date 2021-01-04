3ad2a01a9b052c1b6704b908e7c7a18a702fdbf7
silverioTenor <silverio_tenor@outook.com>
2021-01-04 19:12:52 -0300

    Implementação das configurações de usuários na aplicação e refatoração nos appointments para correta relação de ambas as entidades.

---
+++ src/database/migrations/1609787618013-AlterProviderColumn.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AlterProviderColumn1609787618013
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'appointments_provider_id_fkey',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'appointments',
+      'appointments_provider_id_fkey',
+    );
+  }
+}
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+
+import CreateUsersService from '../services/CreateUser.service';
+
+const usersRouter = Router();
+
+usersRouter.get('/', async (request, response) => {
+  return response.send();
+});
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUsersService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+export default usersRouter;
