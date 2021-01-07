5df5fc4b68b175c9f3ba49dd7824afd8ee73bf29
silverioTenor <silverio_tenor@outook.com>
2021-01-07 14:58:21 -0300

    Implementação de uma rota para visualização dos avatares.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import express from 'express';
 import 'reflect-metadata';
 
-import express from 'express';
-
+import uploadConfig from './config/upload';
 import routes from './routes';
 
 import './database';
@@ -10,5 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => console.log('Server is running in the port 3333...'));
