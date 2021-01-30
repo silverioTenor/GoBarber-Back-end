8052f8bee690db16bbe46217514523954543fd3a
silverioTenor <silverio_tenor@outook.com>
2021-01-30 18:19:28 -0300

    Configurando servidor para comunicação com cliente.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import uploadConfig from './config/upload';
 import routes from './routes';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
