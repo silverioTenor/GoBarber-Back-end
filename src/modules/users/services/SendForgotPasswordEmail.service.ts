716e6a6b6f66a84535f034a57201b1e87b565424
Rodrigo Silvério <silverio_tenor@outlook.com>
2021-03-23 12:26:56 -0300

    Configuração inicial do teste para envio de email e recuperação de senha.

---
+++ src/modules/users/services/SendForgotPasswordEmail.service.ts
@@ -0,0 +1,25 @@
+import { injectable, inject } from 'tsyringe';
+
+import IMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido!');
+  }
+}
+
+export default SendForgotPasswordEmailService;
