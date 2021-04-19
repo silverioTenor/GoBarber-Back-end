a9e317d4cb1e05f93b614d8d0c097c26343c56a0
Rodrigo Silvério <silverio_tenor@outlook.com>
2021-04-19 14:02:54 -0300

    Implementação: serviço de listagem de horários disponíveis em um dia.

---
+++ src/modules/appointments/services/ListProviderDayAvailability.service.spec.ts
@@ -0,0 +1,48 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+
+import ListProviderDayAvailabilityService from './ListProviderDayAvailability.service';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('Should be able to list the day availability from provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 21, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2021, 4, 21, 16, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 4, 21, 11).getTime();
+    });
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id: 'user',
+      day: 21,
+      month: 5,
+      year: 2021,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 13, available: false },
+        { hour: 14, available: true },
+        { hour: 15, available: true },
+        { hour: 16, available: false },
+      ]),
+    );
+  });
+});
